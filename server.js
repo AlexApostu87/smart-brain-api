84cb1bf2ec31712d334eae06b7f8cc1f7c7946db
AlexApostu87 <apostu.alex1987@gmail.com>
2020-10-31 13:14:08 +0200

    changed port from static to dynamic

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
 
 
 
-app.listen(3001, () => {
-	console.log(`Server is listening on port 3001`);
+app.listen(process.env.PORT || 3001, () => {
+	console.log(`Server is listening on port ${process.env.PORT}`);
 });
